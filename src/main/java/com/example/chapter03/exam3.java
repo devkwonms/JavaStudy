05fd94ca284143aa83822f60fa28eea577196c58
devkwonms <alstj1379@nate.com>
2023-05-26 04:59:08 +0900

    변수값을 다시 초기화 후 출력하기

--- src/main/java/com/example/chapter03/exam3.java
+++ src/main/java/com/example/chapter03/exam3.java
@@ -4,5 +4,7 @@ public class exam3 {
     public static void main(String[]args){
         int value = 1234;
         System.out.println(value);
+        value = 5678;
+        System.out.println(value);
     }
 }
