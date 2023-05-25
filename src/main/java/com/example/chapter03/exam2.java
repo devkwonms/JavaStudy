e6de32d4ae9b49cb53814edf4732768ee8646073
devkwonms <alstj1379@nate.com>
2023-05-26 04:36:12 +0900

    변수 naming

---
+++ src/main/java/com/example/chapter03/exam2.java
@@ -0,0 +1,10 @@
+package com.example.chapter03;
+
+public class exam2 {
+    public static void main(String[]args){
+        int price = 100;
+        int quantity = 80;
+        int revenue = price * quantity;
+        System.out.println("오늘 매출은 "+ revenue +"원입니다.");
+    }
+}
