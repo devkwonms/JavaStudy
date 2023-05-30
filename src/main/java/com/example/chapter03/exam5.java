ad442cf8318eed1c3800b3ba2d59ff72fd3f17bc
devkwonms <alstj1379@nate.com>
2023-05-30 19:06:28 +0900

    radix(여러 진수표현)

---
+++ src/main/java/com/example/chapter03/exam5.java
@@ -0,0 +1,15 @@
+package com.example.chapter03;
+
+public class exam5 {
+    public static void main(String[]args){
+        int a = 10;
+        int b = 0x10;
+        int c = 015;
+        int d = 0b1001;
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        System.out.println("c = " + c);
+        System.out.println("d = " + d);
+    }
+}
