cfb0f8c5e9c5d07b185be7e6d1d9759d3dc0c0ea
devkwonms <alstj1379@nate.com>
2023-05-30 19:10:00 +0900

    underbar - 너무 큰 자릿수일 경우 리터럴 중간에 밑줄삽입허용(Java7 이후부터)

---
+++ src/main/java/com/example/chapter03/exam6.java
@@ -0,0 +1,8 @@
+package com.example.chapter03;
+
+public class exam6 {
+    public static void main(String[]args){
+        long price = 12_3456_7890_1234L;
+        System.out.println("price = " + price);
+    }
+}
