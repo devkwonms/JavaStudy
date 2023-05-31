63a458d01e0f1a1ecb5844b711586da5b6eb6767
devkwonms <alstj1379@nate.com>
2023-05-31 18:59:00 +0900

    overflow

---
+++ src/main/java/com/example/chapter03/exam7.java
@@ -0,0 +1,9 @@
+package com.example.chapter03;
+
+public class exam7 {
+    public static void main(String[]args){
+        byte value = 127;
+        value++;
+        System.out.println("value = " + value);
+    }
+}
