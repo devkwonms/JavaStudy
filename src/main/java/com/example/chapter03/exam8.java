d0cbf82fb4a09baea7edac8821ef48fe65e43fb8
devkwonms <alstj1379@nate.com>
2023-06-01 13:11:27 +0900

    overflow2

---
+++ src/main/java/com/example/chapter03/exam8.java
@@ -0,0 +1,10 @@
+package com.example.chapter03;
+
+public class exam8 {
+    public static void main(String[]args){
+        int value = 1234567890;
+        value = value * 2;
+        System.out.println("value = " + value);
+        
+    }
+}
