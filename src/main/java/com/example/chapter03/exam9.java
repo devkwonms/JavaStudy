db20d65d1ff4d7b4e9454ad7b56da205e488b6d3
devkwonms <alstj1379@nate.com>
2023-06-01 13:20:36 +0900

    precision(실수의 정밀도)

---
+++ src/main/java/com/example/chapter03/exam9.java
@@ -0,0 +1,12 @@
+package com.example.chapter03;
+
+public class exam9 {
+    public static void main(String[]args){
+        float value = 12345.123456789f;
+        System.out.println("value = " + value);
+        double value2 = 12345.123456789;
+        System.out.println("value2 = " + value2);
+        double pie = 3.14159265358979323843363279502884;
+        System.out.println("pie = " + pie);
+    }
+}
