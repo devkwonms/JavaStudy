41301744dd0e8f796ab697c8966b23d1c34909c8
devkwonms <alstj1379@nate.com>
2023-06-02 21:29:20 +0900

    char

---
+++ src/main/java/com/example/chapter03/exam11.java
@@ -0,0 +1,14 @@
+package com.example.chapter03;
+
+public class exam11 {
+    public static void main(String[]args){
+        char ch = '한';
+        System.out.println("ch = " + ch);
+        
+        char ch1 = '권';
+        System.out.println("(int)ch1 = " + (int)ch1);
+
+        int i = ch;
+        System.out.println("i = " + i);
+    }
+}
