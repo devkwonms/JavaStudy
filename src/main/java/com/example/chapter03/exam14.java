d7ab61c6ef681a7b45e6010084acf60fbd410c7b
devkwonms <alstj1379@nate.com>
2023-06-03 21:06:46 +0900

    longstring

---
+++ src/main/java/com/example/chapter03/exam14.java
@@ -0,0 +1,11 @@
+package com.example.chapter03;
+
+public class exam14 {
+    public static void main (String[]args){
+        String str = "아름다운 이땅에 금수강산에 " +
+                "단군 할아버지가 \"터\" 잡으시고\n"+
+                "홍익인간 뜻으로 나라 세우니 "+
+                "대대손손 훌륭한 인물도 많아.";
+        System.out.println("str = " + str);
+    }
+}
