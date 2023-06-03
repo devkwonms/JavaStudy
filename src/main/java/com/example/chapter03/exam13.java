1f3ae08e3e51cfff70e2ebc7b93002b4494c17ae
devkwonms <alstj1379@nate.com>
2023-06-03 21:00:59 +0900

    String(참조형, 불변객체지만 대입형식 지원)

---
+++ src/main/java/com/example/chapter03/exam13.java
@@ -0,0 +1,11 @@
+package com.example.chapter03;
+
+public class exam13 {
+    public static void main(String[]args){
+        String str = "대한민국";
+        System.out.println("str = " + str);
+        
+        String name = new String("권민서");
+        System.out.println("name = " + name);
+    }
+}
