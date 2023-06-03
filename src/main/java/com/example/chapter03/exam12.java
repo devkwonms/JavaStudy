fc602c63cf067d70847da87bfbed8c36420d3c99
devkwonms <alstj1379@nate.com>
2023-06-03 20:57:36 +0900

    escape

---
+++ src/main/java/com/example/chapter03/exam12.java
@@ -0,0 +1,14 @@
+package com.example.chapter03;
+
+public class exam12 {
+    public static void main(String[]args){
+        char yen = '\u00a5';
+        System.out.print("yen = " + yen);
+        
+        char omega = '\u03c9';
+        System.out.print("omega = " + omega);
+        System.out.print('\n');
+        char quot = '\'';
+        System.out.println("quot = " + quot);
+    }
+}
