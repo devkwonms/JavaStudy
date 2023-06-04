375e6172be5ee3021b56039e23202163bb64b7df
devkwonms <alstj1379@nate.com>
2023-06-04 21:33:19 +0900

    textblockliteral

---
+++ src/main/java/com/example/chapter03/exam16.java
@@ -0,0 +1,12 @@
+package com.example.chapter03;
+
+public class exam16 {
+    public static void main(String[]args){
+        String str = "HTML example\n" + """
+                <body>
+                    <p>문단</p>
+                </body>
+                """;
+        System.out.println(str);
+    }
+}
