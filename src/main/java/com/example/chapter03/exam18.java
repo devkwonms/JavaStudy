f038426a269ac1de2656e16d00d4787832e3fcf5
devkwonms <alstj1379@nate.com>
2023-06-04 22:03:50 +0900

    boolean

--- src/main/java/com/example/chapter03/exam18.java
+++ src/main/java/com/example/chapter03/exam18.java
@@ -4,13 +4,16 @@ public class exam18 {
     public static void main(String[]args){
         int a = 3;
         boolean b = (a==3);
-        int c = 4;
+        int c = 4; int d = 5;
         if(b){
             System.out.println("b = " + b);
             System.out.println("a가 3이다");
         }
         if(c ==4){
             System.out.println("c는 4야!");
+        }
+        if(d ==5){
+            System.out.println("d는 5야!");
         }
     }
 }
