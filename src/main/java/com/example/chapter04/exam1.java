c8345b248464807f46a33daf1741c6cb741be97b
devkwonms <alstj1379@nate.com>
2023-06-05 00:33:50 +0900

    if

---
+++ src/main/java/com/example/chapter04/exam1.java
@@ -0,0 +1,11 @@
+package com.example.chapter04;
+
+public class exam1 {
+    public static void main(String[] args) {
+        int age = 21;
+
+        if(age >= 19){
+            System.out.println("성인입니다");
+        }
+    }
+}
