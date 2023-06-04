9912c654052d79f561aae4fa5a36bfdb6399e17f
devkwonms <alstj1379@nate.com>
2023-06-04 22:00:39 +0900

    boolean

---
+++ src/main/java/com/example/chapter03/exam18.java
@@ -0,0 +1,12 @@
+package com.example.chapter03;
+
+public class exam18 {
+    public static void main(String[]args){
+        int a = 3;
+        boolean b = (a==3);
+        if(b){
+            System.out.println("b = " + b);
+            System.out.println("a가 3이다");
+        }
+    }
+}
