fc7c867192332ef571c5fdc3bd5b0aa788deee5a
devkwonms <alstj1379@nate.com>
2023-06-06 17:27:19 +0900

    ifelse

---
+++ src/main/java/com/example/chapter04/exam3.java
@@ -0,0 +1,13 @@
+package com.example.chapter04;
+
+public class exam3 {
+    public static void main(String[]args){
+        int age = 15;
+        if(age >= 19){
+            System.out.println("성인입니다!");
+            System.out.println("술집입장!");
+        }else{
+            System.out.println("미성년자입니다!");
+        }
+    }
+}
