2c9665ac80e565dcefff9cc2ce3f539647d62232
devkwonms <alstj1379@nate.com>
2023-06-08 07:32:51 +0900

    switch의 필요성(if else if 가 많다보면 형식성이 다소 떨어짐)

---
+++ src/main/java/com/example/chapter04/exam6.java
@@ -0,0 +1,16 @@
+package com.example.chapter04;
+
+public class exam6 {
+    public static void main(String[]args){
+        int ranking = 2;
+        if(ranking == 1){
+            System.out.println("금메달 축하합니다!");
+        }else if(ranking ==2){
+            System.out.println("은메달 축하합니다!");
+        }else if(ranking ==3){
+            System.out.println("동메달 축하합니다!");
+        }else{
+            System.out.println("참가상을 드립니다!");
+        }
+    }
+}
