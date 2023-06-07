31f824b6bd6a839ca6b24bb8350cbc80cc51c120
devkwonms <alstj1379@nate.com>
2023-06-08 07:50:01 +0900

    switch의 제어변수가 문자열일때(Java7부터~)

---
+++ src/main/java/com/example/chapter04/exam10.java
@@ -0,0 +1,19 @@
+package com.example.chapter04;
+
+public class exam10 {
+    public static void main(String[]args){
+        String country = "China";
+
+        switch (country){
+            case "Korea":
+                System.out.println("Seoul!");
+                break;
+            case "China":
+                System.out.println("BeiJing!");
+                break;
+            case "Japan":
+                System.out.println("ToKyo!");
+                break;
+        }
+    }
+}
