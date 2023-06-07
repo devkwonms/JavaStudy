c1eb1a2670c9aa6d7e24d403126cfc1c9a7210a5
devkwonms <alstj1379@nate.com>
2023-06-08 07:23:25 +0900

    ifElseIf

---
+++ src/main/java/com/example/chapter04/exam4.java
@@ -0,0 +1,15 @@
+package com.example.chapter04;
+
+public class exam4 {
+    public static void main(String[]args){
+        int age = 16;
+        
+        if(age>= 19){
+            System.out.println("성인입니다!");
+        }else if(age > 12){
+            System.out.println("청소년입니다!");
+        }else{
+            System.out.println("어린이입니다!");
+        }
+    }
+}
