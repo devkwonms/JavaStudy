41635f2d48fe368f01d39bfb1dbbb76a89b284cc
devkwonms <alstj1379@nate.com>
2023-06-08 19:39:17 +0900

    oddsum

--- src/main/java/com/example/chapter05/exam2.java
+++ src/main/java/com/example/chapter05/exam2.java
@@ -4,7 +4,7 @@ public class exam2 {
     public static void main(String[]args){
         int sum = 0;
 
-        for(int i =1; i<=100; i++){
+        for(int i =1; i<=100; i = i+2){
             sum = sum + i;
         }
         System.out.println("합계 = " + sum);
