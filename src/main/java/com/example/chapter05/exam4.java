db5fa61cb58fa312a8e3cfc6626bab54b6fad425
devkwonms <alstj1379@nate.com>
2023-06-08 19:57:38 +0900

    twocontrol

---
+++ src/main/java/com/example/chapter05/exam4.java
@@ -0,0 +1,9 @@
+package com.example.chapter05;
+
+public class exam4 {
+    public static void main(String[]args){
+        for(int i=0, j=1; i<5; i++, j= j+2){
+            System.out.println("i = " + i + " j = " + j );
+        }
+    }
+}
