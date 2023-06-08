393a1cb12da2114e8e479dd3d5af46e083c89d2a
devkwonms <alstj1379@nate.com>
2023-06-08 19:51:25 +0900

    floatloop

---
+++ src/main/java/com/example/chapter05/exam3.java
@@ -0,0 +1,14 @@
+package com.example.chapter05;
+
+public class exam3 {
+    public static void main(String[]args){
+        for(int i = 10; i>0; i--){
+            System.out.print(i +",");
+        }
+        System.out.println();
+
+        for(double d =0; d < 3.0 ; d = d+0.5){
+            System.out.print(d +",");
+        }
+    }
+}
