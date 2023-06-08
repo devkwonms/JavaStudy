d5553a55a16a82b838916f257f79a5e2fb78c41b
devkwonms <alstj1379@nate.com>
2023-06-08 09:19:21 +0900

    rangecase- case문의 값은 상수여야함

---
+++ src/main/java/com/example/chapter04/exam12.java
@@ -0,0 +1,26 @@
+package com.example.chapter04;
+
+public class exam12 {
+    public static void main(String[]args){
+        int score = 100;
+
+        switch (score/10){
+            case 10:
+            case 9:
+                System.out.println("A");
+                break;
+            case 8:
+                System.out.println("B");
+                break;
+            case 7:
+                System.out.println("C");
+                break;
+            case 6:
+                System.out.println("D");
+                break;
+            default:
+                System.out.println("F");
+                break;
+        }
+    }
+}
