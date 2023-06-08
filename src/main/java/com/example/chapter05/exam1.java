7a5970022db7902e4f2395710c81522c4f2da871
devkwonms <alstj1379@nate.com>
2023-06-08 19:31:42 +0900

    for

---
+++ src/main/java/com/example/chapter05/exam1.java
@@ -0,0 +1,9 @@
+package com.example.chapter05;
+
+public class exam1 {
+    public static void main(String[]args){
+        for(int i =1; i<=5;i++){
+            System.out.println(i+"번 학생 성적처리");
+        }
+    }
+}
