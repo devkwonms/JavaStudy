c4eb1d63ca508322f4c76d379e3476fbd3a2883f
devkwonms <alstj1379@nate.com>
2023-06-13 03:42:35 +0900

    triangle exam

--- src/main/java/com/example/chapter05/exam12.java
+++ src/main/java/com/example/chapter05/exam12.java
@@ -4,11 +4,7 @@ public class exam12 {
     public static void main(String[] args) {
         for(int i = 1; i <=10; i++){
             for(int j = 1; j <=10; j++){
-                if(j > 10-i){
-                    System.out.print("*");
-                }else{
-                    System.out.print(" ");
-                }
+                System.out.print(j > 10-i ? "*" : " ");
             }
             System.out.println();
         }
