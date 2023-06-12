5bf3404219dc116bf3012a1da845bc6c24c18b99
devkwonms <alstj1379@nate.com>
2023-06-13 02:51:05 +0900

    triangle

---
+++ src/main/java/com/example/chapter05/exam10.java
@@ -0,0 +1,12 @@
+package com.example.chapter05;
+
+public class exam10 {
+    public static void main(String[]args){
+        for(int i = 1; i<=10; i++){
+            for(int j = 0; j < i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
