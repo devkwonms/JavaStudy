71fd834b7084bcfb0f38bd9977c09318e89b3348
devkwonms <alstj1379@nate.com>
2023-06-14 17:10:25 +0900

    알파벳 피라미드

--- src/main/java/com/example/chapter05/exam27.java
+++ src/main/java/com/example/chapter05/exam27.java
@@ -1,10 +1,15 @@
 package com.example.chapter05;
 
 public class exam27 {
-    public static void main(String[]args){
-        for(int i = 1; i <= 10; i ++){
-            for(int j = 0; j< i; j ++){
-                System.out.print((char)(i+96));
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            for (int j = 0; j < 10 - i; j++) {
+//                System.out.print((char)(i+96));
+                System.out.print(" ");
+            }
+            for (int j = 0; j < 2 * i - 1; j++) {
+                System.out.print((char) (i + 96));
+
             }
             System.out.println();
         }
