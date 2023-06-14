defb6ebb2a577236d08295449b0373ffe66c3d32
devkwonms <alstj1379@nate.com>
2023-06-14 16:47:10 +0900

    10~50 정수합계

---
+++ src/main/java/com/example/chapter05/exam26.java
@@ -0,0 +1,11 @@
+package com.example.chapter05;
+
+public class exam26 {
+    public static void main(String[]args){
+        int sum = 0;
+        for(int i = 10; i <=50; i ++){
+            sum = sum + i;
+        }
+        System.out.println("sum = " + sum);
+    }
+}
