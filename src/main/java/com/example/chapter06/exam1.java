a85f6b2504bcc34ac4f0c6dfc0cd0dfef041a70c
devkwonms <alstj1379@nate.com>
2023-06-15 18:20:35 +0900

    arithmetic

---
+++ src/main/java/com/example/chapter06/exam1.java
@@ -0,0 +1,12 @@
+package com.example.chapter06;
+
+public class exam1 {
+    public static void main(String[]args){
+        int a = 6, b = 4;
+        System.out.println(a + " + " + b + " = " + (a + b));
+        System.out.println(a + " - " + b + " = " + (a - b));
+        System.out.println(a + " * " + b + " = " + (a * b));
+        System.out.println(a + " / " + b + " = " + (a / b));
+        System.out.println(a + " / " + b + " = " + ((float)a / b));
+    }
+}
