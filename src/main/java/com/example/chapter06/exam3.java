3055e8aed722b0abe37d3274f39c87447e599c03
devkwonms <alstj1379@nate.com>
2023-06-15 23:57:03 +0900

    plusequal

---
+++ src/main/java/com/example/chapter06/exam3.java
@@ -0,0 +1,11 @@
+package com.example.chapter06;
+
+public class exam3 {
+    public static void main(String[]args){
+        int value = 2;
+        value += 3;
+        System.out.println("value = " + value);
+        value *=2;
+        System.out.println("value = " + value);
+    }
+}
