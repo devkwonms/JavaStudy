28ff7264b67fd6d89bcc9d91fb1fe8bc17c50101
devkwonms <alstj1379@nate.com>
2023-06-16 02:21:27 +0900

    stringcat2

---
+++ src/main/java/com/example/chapter06/exam6.java
@@ -0,0 +1,9 @@
+package com.example.chapter06;
+
+public class exam6 {
+    public static void main(String[] args) {
+        System.out.println("응답하라 " + 19 + 89);
+        System.out.println(19 + 89 + " 응답하라");
+        System.out.println("" + 19 + 89 + " 응답하라");
+    }
+}
