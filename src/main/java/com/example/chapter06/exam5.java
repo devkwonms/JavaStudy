a8216b08cbc85555dc65ad1ca9716246619e8a9a
devkwonms <alstj1379@nate.com>
2023-06-16 02:05:21 +0900

    stringcat

---
+++ src/main/java/com/example/chapter06/exam5.java
@@ -0,0 +1,13 @@
+package com.example.chapter06;
+
+public class exam5 {
+    public static void main(String[] args) {
+        System.out.println(4 + 5);
+        System.out.println("영구와 " + "땡칠이");
+        System.out.println("응답하라 " + 1989);
+        System.out.println(1989 + " 응답하라");
+
+        double root2 = 1.414;
+        System.out.println("2의 제곱근: " + root2);
+    }
+}
