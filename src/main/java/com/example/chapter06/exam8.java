a463236d3e3cf857e4038795b4cb6a749f5f9df2
devkwonms <alstj1379@nate.com>
2023-06-16 03:19:18 +0900

    absolute

---
+++ src/main/java/com/example/chapter06/exam8.java
@@ -0,0 +1,11 @@
+package com.example.chapter06;
+
+public class exam8 {
+    public static void main(String[]args){
+        int value = -8;
+        if(value < 0){
+            value = -value;
+        }
+        System.out.println("value = " + value);
+    }
+}
