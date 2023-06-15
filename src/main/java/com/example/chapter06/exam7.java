9c2cff6f113bfff47517230e7100df40c6b52995
devkwonms <alstj1379@nate.com>
2023-06-16 02:25:19 +0900

    sign

---
+++ src/main/java/com/example/chapter06/exam7.java
@@ -0,0 +1,11 @@
+package com.example.chapter06;
+
+public class exam7 {
+    public static void main(String[]args){
+        int value = 8;
+        System.out.println("value = " + value);
+
+        value = -value;
+        System.out.println("value = " + value);
+    }
+}
