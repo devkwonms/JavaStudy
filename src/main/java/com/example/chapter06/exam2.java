43ae8533462a58b9335addb8e2a24414b323e0a1
devkwonms <alstj1379@nate.com>
2023-06-15 19:28:16 +0900

    modular

---
+++ src/main/java/com/example/chapter06/exam2.java
@@ -0,0 +1,11 @@
+package com.example.chapter06;
+
+public class exam2 {
+    public static void main(String[]args){
+        System.out.println("6 %% 4 = " + 6 % 4);    // 2
+        System.out.println("7 %% 3 = " + 7 % 3);    // 1
+        System.out.println("5.0 %% 2.3 = " + 5.0 % 2.3); // 0.4xx
+        System.out.println("14 %% 2 = " + 14 % 2);  // 0
+        System.out.println("15 %% 2 = " + 15 % 2); // 1
+    }
+}
