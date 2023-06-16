2adf423d1a3aeda0210b42f0f933e7814a522df6
devkwonms <alstj1379@nate.com>
2023-06-16 21:14:45 +0900

    getabs

---
+++ src/main/java/com/example/chapter06/exam10.java
@@ -0,0 +1,9 @@
+package com.example.chapter06;
+
+public class exam10 {
+    public static void main(String[] args) {
+        int value = -5;
+
+        System.out.println("절댓값 = " + (value > 0 ? value : -value));
+    }
+}
