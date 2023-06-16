b598113b3548d39d9dac7fc4c3d27c321d5c6bea
devkwonms <alstj1379@nate.com>
2023-06-16 18:55:26 +0900

    논리 연산자 and

---
+++ src/main/java/com/example/chapter06/exam9.java
@@ -0,0 +1,13 @@
+package com.example.chapter06;
+
+public class exam9 {
+    public static void main(String[] args) {
+        int value = 7;
+
+        if (value > 5 && value < 10) {
+            System.out.println("범위 안에 있음");
+        } else {
+            System.out.println("범위 안에 없음");
+        }
+    }
+}
