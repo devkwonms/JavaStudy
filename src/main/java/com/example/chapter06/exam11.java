749e6586a66c58b28b8df3e1d9594a75dd8e2663
devkwonms <alstj1379@nate.com>
2023-06-16 21:17:22 +0900

    join(삼항연산자)

---
+++ src/main/java/com/example/chapter06/exam11.java
@@ -0,0 +1,9 @@
+package com.example.chapter06;
+
+public class exam11 {
+    public static void main(String[] args) {
+        int value = 6;
+
+        System.out.println("value는 " + (value % 2 == 0 ? "짝" : "홀") + "수입니다.");
+    }
+}
