eaaef5f698fb06daf9cf12bd01836d633b098def
devkwonms <alstj1379@nate.com>
2023-06-19 20:22:38 +0900

    stringint(캐스팅)

---
+++ src/main/java/com/example/chapter06/exam12.java
@@ -0,0 +1,13 @@
+package com.example.chapter06;
+
+public class exam12 {
+    public static void main(String[]args){
+        String num = "123";
+        int value = Integer.parseInt(num);
+        value++;
+        System.out.println("정수 = " + value);
+
+        String str = String.valueOf(value);
+        System.out.println("문자열 = " + str);
+    }
+}
