6e392d4a6a1c74035df2f930a3dbbd787f3a848b
devkwonms <alstj1379@nate.com>
2023-06-20 19:51:25 +0900

    contassign

---
+++ src/main/java/com/example/chapter06/exam13.java
@@ -0,0 +1,10 @@
+package com.example.chapter06;
+
+public class exam13 {
+    public static void main(String[]args){
+        int a,b,c,d;
+        a = b = c = d = 5;
+
+        System.out.println("c = " + c);
+    }
+}
