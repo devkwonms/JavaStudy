4ebe68a0ae1cfbb5a5ad9dd8f943948cac434b9f
devkwonms <alstj1379@nate.com>
2023-06-21 20:22:27 +0900

    rectangular(2차원배열)

---
+++ src/main/java/com/example/chapter07/exam5.java
@@ -0,0 +1,18 @@
+package com.example.chapter07;
+
+public class exam5 {
+    public static void main(String[] args) {
+        int[][] score = {
+                {77, 56, 70, 82},
+                {99, 96, 89, 88},
+                {81, 69, 62, 80}
+        };
+        for (int student = 0; student < score.length; student++) {
+            System.out.print((student + 1) + "번 학생의 성적: ");
+            for (int subject = 0; subject < score[0].length; subject++) {
+                System.out.print(score[student][subject] + " ");
+            }
+            System.out.println("");
+        }
+    }
+}
