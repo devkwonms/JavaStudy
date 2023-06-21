7a61c33507b9f79fd45913bab359485b773ae7e4
devkwonms <alstj1379@nate.com>
2023-06-21 19:33:47 +0900

    andor

---
+++ src/main/java/com/example/chapter06/exam14.java
@@ -0,0 +1,16 @@
+package com.example.chapter06;
+
+public class exam14 {
+    public static void main(String[]args){
+        int a = 1;
+        int b = 2;
+        int c = 3;
+        int d = 4;
+
+        if(a == 1 || b == 2 && c == 10 || d == 10) {
+            System.out.println("진실");
+        }else{
+            System.out.println("거짓");
+        }
+    }
+}
