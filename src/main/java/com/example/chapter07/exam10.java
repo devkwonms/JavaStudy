0956ff1fb35f5a5cfbb123ce8979ca31642b5615
devkwonms <alstj1379@nate.com>
2023-06-25 17:45:17 +0900

    copyprim

---
+++ src/main/java/com/example/chapter07/exam10.java
@@ -0,0 +1,12 @@
+package com.example.chapter07;
+
+public class exam10 {
+    public static void main(String[]args){
+        int i = 1;
+        int i2 = i;
+
+        i2= 1000;
+        System.out.println("i = " + i);
+        System.out.println("i2 = " + i2);
+    }
+}
