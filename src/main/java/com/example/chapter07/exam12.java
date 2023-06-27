6c2deaeb06666fec309511fcaaafc284cb5b1ef3
devkwonms <alstj1379@nate.com>
2023-06-27 18:04:05 +0900

    compareRef

---
+++ src/main/java/com/example/chapter07/exam12.java
@@ -0,0 +1,9 @@
+package com.example.chapter07;
+
+public class exam12 {
+    public static void main(String[]args){
+        int i = 123, i2 = 123;
+        int[] ar = {1,2,3}, ar2 = {1,2,3};
+        int[] ar3 = ar;
+    }
+}
