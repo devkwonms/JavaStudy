5aff0fcacb48d06d2b5400572853c960cdcf9873
devkwonms <alstj1379@nate.com>
2023-06-29 21:16:01 +0900

    null

---
+++ src/main/java/com/example/chapter07/exam13.java
@@ -0,0 +1,12 @@
+package com.example.chapter07;
+
+public class exam13 {
+    public static void main(String[]args){
+
+        int[] ar = {1, 2, 3};
+        System.out.println(ar[0]);
+
+        ar = null;
+        System.out.println(ar[0]);
+    }
+}
