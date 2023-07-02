6d2c57f2ace2f0534ce3662ed302c7d809bc5213
devkwonms <alstj1379@nate.com>
2023-07-02 18:09:20 +0900

    참조형(배열)의 주소 확인

--- src/main/java/com/example/chapter07/exam12.java
+++ src/main/java/com/example/chapter07/exam12.java
@@ -6,6 +6,10 @@ public class exam12 {
         int[] ar = {1, 2, 3}, ar2 = {1, 2, 3};
         int[] ar3 = ar;
 
+        System.out.println("ar = " + ar);
+        System.out.println("ar2 = " + ar2);
+        System.out.println("ar3 = " + ar3);
+
         System.out.println("i와 i2 : " + (i == i2 ? "같다" : "다르다"));
         System.out.println("ar와 ar2 : " + (ar == ar2 ? "같다" : "다르다"));
         System.out.println("ar와 ar3 : " + (ar == ar3 ? "같다" : "다르다"));
