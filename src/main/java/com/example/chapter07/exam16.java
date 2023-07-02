9682e3f10b6cb28bcc34876a784cc0f183a7615b
devkwonms <alstj1379@nate.com>
2023-07-02 18:34:45 +0900

    lookup2

---
+++ src/main/java/com/example/chapter07/exam16.java
@@ -0,0 +1,16 @@
+package com.example.chapter07;
+
+public class exam16 {
+    public static void main(String[] args) {
+        int rank = 2;
+        String[] message = {
+                "",
+                "최고의 성적입니다. 축하합니다.",
+                "우수한 성적입니다.",
+                "보통입니다.",
+                "다소 부진합니다. 더 노력하세요",
+                "왜 사니?",
+        };
+        System.out.println(message[rank]);
+    }
+}
