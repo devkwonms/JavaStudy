b7e4ed722f61eee85d1440487380d16d193e9a7b
devkwonms <alstj1379@nate.com>
2023-07-06 14:50:51 +0900

    boxmessage

--- src/main/java/com/example/chapter08/exam11.java
+++ src/main/java/com/example/chapter08/exam11.java
@@ -1,7 +1,24 @@
 package com.example.chapter08;
 
 public class exam11 {
-    public static void main(String[]args){
+    public static void main(String[] args) {
+        outCharsln('-', 5 + 4);
+        System.out.println("| Hello |");
+        outCharsln('-', 5 + 4);
 
+        outCharsln('-', 14 + 4);
+        System.out.println("| Congratulation |");
+        outCharsln('-', 14 + 4);
+    }
+
+    static void outChars(char ch, int num) {
+        for (int i = 0; i < num; i++) {
+            System.out.print(ch);
+        }
+    }
+
+    static void outCharsln(char ch, int num) {
+        outChars(ch, num);
+        System.out.println();
     }
 }
