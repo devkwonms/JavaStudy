eefff82deb7c81543c3eccb474b6fb7f8ca24ce2
devkwonms <alstj1379@nate.com>
2023-07-08 20:04:43 +0900

    thread

--- src/main/java/com/example/mission/problem1.java
+++ src/main/java/com/example/mission/problem1.java
@@ -2,25 +2,32 @@ package com.example.mission;
 
 public class problem1 {
     public static void main(String[] args) {
-        Thread thread1 = new Thread(new MyRunnable("Thread 1"));
-        Thread thread2 = new Thread(new MyRunnable("Thread 2"));
+//        Thread thread1 = new Thread(new MyRunnable("Thread 1"));
+//        Thread thread2 = new Thread(new MyRunnable("Thread 2"));
+//
+//        thread2.run();
+//        // 쓰레드 수행
+//        thread1.start();
+//        thread2.start();
+//
+//        // 기타 작업을 수행할 수 있습니다.
+//
+//        try {
+//        // 다른 스레드의 실행이 끝날 때까지 현재 스레드를 일시 정지
+//            thread1.join();
+//            thread2.join();
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//
+//        System.out.println("모든 스레드가 종료되었습니다.");
 
-        thread2.run();
-        // 쓰레드 수행
-        thread1.start();
-        thread2.start();
+        final int THREAD_COUNT = 100000;
 
-        // 기타 작업을 수행할 수 있습니다.
-
-        try {
-        // 다른 스레드의 실행이 끝날 때까지 현재 스레드를 일시 정지
-            thread1.join();
-            thread2.join();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+        for (int i = 0; i < THREAD_COUNT; i++) {
+            Thread thread = new Thread(new MyRunnable("Thread" + i));
+            thread.start();
         }
-
-        System.out.println("모든 스레드가 종료되었습니다.");
     }
 }
 
