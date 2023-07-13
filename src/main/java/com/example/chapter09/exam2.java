e7a685ca2815082193d61aa3396499ef56509d41
devkwonms <alstj1379@nate.com>
2023-07-13 20:22:01 +0900

    timector

--- src/main/java/com/example/chapter09/exam2.java
+++ src/main/java/com/example/chapter09/exam2.java
@@ -6,6 +6,13 @@ class Time {
     int minute;
     int second;
 
+    Time(boolean am, int hour, int minute, int second) {
+        this.am = am;
+        this.hour = hour;
+        this.minute = minute;
+        this.second = second;
+    }
+
     void whatTime() {
         System.out.print(am ? "오전 " : "오후 ");
         System.out.println(hour + "시 " + minute + "분 " + second + "초");
@@ -14,12 +21,7 @@ class Time {
 
 public class exam2 {
     public static void main(String[] args) {
-        Time now = new Time();
-        now.am = true;
-        now.hour = 12;
-        now.minute = 34;
-        now.second = 56;
-
+        Time now = new Time(true, 12, 34, 56);
         now.whatTime();
     }
 }
