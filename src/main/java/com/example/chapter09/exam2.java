9ee54eae456d6c4d37867309bf4d42faae0c1460
devkwonms <alstj1379@nate.com>
2023-07-14 03:15:43 +0900

    ctorover

--- src/main/java/com/example/chapter09/exam2.java
+++ src/main/java/com/example/chapter09/exam2.java
@@ -6,11 +6,28 @@ class Time {
     int minute;
     int second;
 
+    // 인수가 4개인 생성자
     Time(boolean am, int hour, int minute, int second) {
         this.am = am;
         this.hour = hour;
         this.minute = minute;
         this.second = second;
+    }
+
+    // 인수가 3개인 생성자
+    Time(boolean am, int hour, int minute){
+        this.am = am;
+        this.hour = hour;
+        this.minute = minute;
+        this.second = 0;
+    }
+
+    // 인수가 2개인 생성자
+    Time(int hour24, int minute){
+        this.am = hour24 < 12;
+        this.hour = hour24 % 12;
+        this.minute = minute;
+        this.second = 0;
     }
 
     void whatTime() {
@@ -21,7 +38,14 @@ class Time {
 
 public class exam2 {
     public static void main(String[] args) {
-        Time now = new Time(true, 12, 34, 56);
-        now.whatTime();
+
+        Time now1 = new Time(true, 12, 34, 56); // 오전 12시 34분 56초
+        now1.whatTime();
+
+        Time now2 = new Time(true, 12, 34); // 오전 12시 34분 0초
+        now2.whatTime();
+
+        Time today = new Time(18, 30);  // 오후 6시 30분 0초
+        today.whatTime();
     }
 }
