651336c5663db764bcf72d272e4d4daa3b6b9e09
devkwonms <alstj1379@nate.com>
2023-07-18 02:02:55 +0900

    staticMethod

--- src/main/java/com/example/chapter10/exam3.java
+++ src/main/java/com/example/chapter10/exam3.java
@@ -10,6 +10,20 @@ class Car {
         gasoline = aGasoline;
         carNum++;
     }
+    static void resetNum(){
+        carNum = 0;
+    }
+    static void printNum(){
+        System.out.println("현재 출고 대수 : " + carNum);
+    }
+    
+    void run(){
+        if(gasoline){
+            System.out.println("부릉부릉");
+        }else{
+            System.out.println("덜컹덜컹");
+        }
+    }
 }
 
 public class exam3 {
@@ -17,13 +31,20 @@ public class exam3 {
 
         System.out.println("생성전 : " + Car.carNum);
 
+        Car.printNum();
+
         Car korando = new Car("코란도C", false);
         System.out.println(korando.name + " : " + Car.carNum);
 
         Car equus = new Car("에꿍스", true);
         System.out.println(equus.name + " : " + Car.carNum);
 
+        Car.printNum();
+        Car.resetNum();
+
         Car pride = new Car("프라이드", true);
         System.out.println(pride.name + " : " + Car.carNum);
+
+        Car.printNum();
     }
 }
