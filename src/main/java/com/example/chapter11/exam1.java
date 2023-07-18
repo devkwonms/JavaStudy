6f766b25da1e54038c20229e14b42af7c7ecf684
devkwonms <alstj1379@nate.com>
2023-07-19 02:01:12 +0900

    super(부모의 여러 생성자 중 골라서 각각 객체만들어보기)

--- src/main/java/com/example/chapter11/exam1.java
+++ src/main/java/com/example/chapter11/exam1.java
@@ -9,6 +9,12 @@ class Human {
         this.name = name;
     }
 
+    Human(float birth, String name) {
+        this(0, name);
+        java.time.LocalDate now = java.time.LocalDate.now();
+        this.age = now.getYear() - (int) birth + 1;
+    }
+
     void intro() {
         System.out.println("안녕," + age + "살 " + name + "입니다.");
     }
@@ -20,6 +26,12 @@ class Student extends Human {
 
     Student(int age, String name, int stnum, String major) {
         super(age, name);   // 부모의 생성자에게 초기화 위임
+        this.stnum = stnum;
+        this.major = major;
+    }
+
+    Student(float birth, String name, int stnum, String major) {
+        super(birth, name);
         this.stnum = stnum;
         this.major = major;
     }
@@ -45,16 +57,23 @@ class Graduate extends Student {
 
 public class exam1 {
     public static void main(String[] args) {
-//        Human kwon = new Human(29, "권민서");
-//        kwon.intro();
-//
-//        Student lee = new Student(29, "이우림", 950403, "컴공");
-//        lee.intro();
-//        lee.study();
+        Human kwon = new Human(29, "권민서");
+        kwon.intro();
+
+        Student lee = new Student(29, "이우림", 950403, "컴공");
+        lee.intro();
+        lee.study();
 
         Graduate park = new Graduate(35, "박대희", 95001122, "전산", "웹 보안에 대한 연구");
         park.intro();
         park.study();
         park.research();
+
+        Student kim = new Student(39, "노정란", 9908123, "건축");
+        kim.intro();
+
+        Student bae = new Student(1989.10f, "배지영", 1125034, "간호");
+        bae.intro();
+
     }
 }
