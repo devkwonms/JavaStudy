c2da95fddff564d209a2fa0a2537ffdd25c8e49b
devkwonms <alstj1379@nate.com>
2023-07-18 18:55:51 +0900

    student

---
+++ src/main/java/com/example/chapter11/exam1.java
@@ -0,0 +1,42 @@
+package com.example.chapter11;
+
+class Human {
+    int age;
+    String name;
+
+    Human(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+
+    void intro() {
+        System.out.println("안녕," + age + "살 " + name + "입니다.");
+    }
+}
+
+class Student extends Human {
+    int stnum;
+    String major;
+
+    Student(int age, String name, int stnum, String major) {
+        super(age, name);
+        this.stnum = stnum;
+        this.major = major;
+    }
+
+    void study() {
+        System.out.println("공부를 합니다.");
+    }
+}
+
+public class exam1 {
+    public static void main(String[] args) {
+        Human kwon = new Human(29, "권민서");
+        kwon.intro();
+
+        Student lee = new Student(29, "이우림", 950403, "컴공");
+        lee.intro();
+        lee.study();
+
+    }
+}
