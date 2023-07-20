e1723b7fcdb588e7b6e533f2010dd6d08992515d
devkwonms <alstj1379@nate.com>
2023-07-20 17:50:04 +0900

    objectGroup

--- src/main/java/com/example/chapter11/exam1.java
+++ src/main/java/com/example/chapter11/exam1.java
@@ -21,7 +21,7 @@ class Notebook {
 class Human {
     int age;
     String name;
-    Notebook book = new Notebook("",0,0);
+    Notebook book = new Notebook("", 0, 0);
 
     Human(int age, String name) {
         this.age = age;
@@ -85,6 +85,11 @@ class Soldier extends Human {
     void fight() {
         System.out.println("따콩 따콩. 앞으로 전진!!");
     }
+
+    @Override
+    void intro() {
+        System.out.println("충성. 군번 : " + milnum + "입니다.");
+    }
 }
 
 class Thief extends Human {
@@ -99,6 +104,11 @@ class Thief extends Human {
 
     void steal() {
         System.out.println("살금살금~~ 후다닥~~");
+    }
+
+    @Override
+    void intro() {
+        System.out.println(item + " 전문 털이범 전과 " + star + "범입니다.");
     }
 }
 
@@ -118,45 +128,60 @@ class Graduate extends Student {
 
 public class exam1 {
     public static void main(String[] args) {
-        Human kwon = new Human(29, "권민서");
-        kwon.intro();
+//        Human kwon = new Human(29, "권민서");
+//        kwon.intro();
+//
+//        Student lee = new Student(29, "이우림", 950403, "컴공");
+//        lee.intro();
+//        lee.study();
+//
+//        Graduate park = new Graduate(35, "박대희", 95001122, "전산", "웹 보안에 대한 연구");
+//        park.intro();
+//        park.study();
+//        park.research();
+//
+//        Student kim = new Student(39, "노정란", 9908123, "건축");
+//        kim.intro();
+//
+//        Student bae = new Student(1989.10f, "배지영", 1125034, "간호");
+//        bae.intro();
+//
+//        Student choi = new Student(35, "최현석", 150629, "주자학");
+//        choi.study();
+//        choi.intro();
+//
+//        Soldier kang = new Soldier(45, "강감찬", 12345);
+//        kang.fight();
+//
+//        Thief hong = new Thief(15, "홍길동", "부잣집", 2);
+//        hong.steal();
+//
+//        Human song = new Human(29, "송지훈", "i7 Core", 8, 2000);
+//        song.intro();
+//
+//        Human baek = new Human(29, "백상형");
+//        Student cho = new Student(42, "조승우", 9312345, "경영");
+//
+//        introHuman(baek);
+//        introHuman(cho);
+        Human[] arHuman = {
+                new Human(29, "김상형"),
+                new Student(42, "이승우", 9312345, "경영"),
+                new Soldier(45, "강감찬", 12345),
+                new Thief(15, "홍길동", "부잣집", 2),
+        };
 
-        Student lee = new Student(29, "이우림", 950403, "컴공");
-        lee.intro();
-        lee.study();
-
-        Graduate park = new Graduate(35, "박대희", 95001122, "전산", "웹 보안에 대한 연구");
-        park.intro();
-        park.study();
-        park.research();
-
-        Student kim = new Student(39, "노정란", 9908123, "건축");
-        kim.intro();
-
-        Student bae = new Student(1989.10f, "배지영", 1125034, "간호");
-        bae.intro();
-
-        Student choi = new Student(35, "최현석", 150629, "주자학");
-        choi.study();
-        choi.intro();
-
-        Soldier kang = new Soldier(45, "강감찬", 12345);
-        kang.fight();
-
-        Thief hong = new Thief(15, "홍길동", "부잣집", 2);
-        hong.steal();
-
-        Human song = new Human(29, "송지훈", "i7 Core", 8, 2000);
-        song.intro();
-
-        Human baek = new Human(29, "백상형");
-        Student cho = new Student(42, "조승우", 9312345, "경영");
-
-        introHuman(baek);
-        introHuman(cho);
+        dumpGroup(arHuman);
 
     }
-    static void introHuman(Human who){
+
+    static void introHuman(Human who) {
         who.intro();
+    }
+
+    static void dumpGroup(Human[] group) {
+        for (Human h : group) {
+            h.intro();
+        }
     }
 }
