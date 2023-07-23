e5eaf1e4d199d56440601a729a5968c7ffd79fb9
devkwonms <alstj1379@nate.com>
2023-07-23 19:55:56 +0900

    SmartPhone

--- src/main/java/com/example/chapter13/exam3.java
+++ src/main/java/com/example/chapter13/exam3.java
@@ -14,9 +14,26 @@ interface Camera {
     void takePicture();
 }
 
+interface Mp3 {
+    void play();
+}
+
 class HandPhoneCamera extends HandPhone implements Camera {
     public void takePicture() {
         System.out.println("찰칵. 사진을 찍는다.");
+    }
+}
+
+class SmartPhone extends HandPhone implements Camera, Mp3 {
+
+    @Override
+    public void takePicture() {
+        System.out.println("찰칵. 사진을 찍는다");
+    }
+
+    @Override
+    public void play() {
+        System.out.println("띵가띵가. 음악재생");
     }
 }
 
@@ -25,5 +42,10 @@ public class exam3 {
         HandPhoneCamera myphone = new HandPhoneCamera();
         myphone.call();
         myphone.takePicture();
+
+        SmartPhone galaxy = new SmartPhone();
+        galaxy.call();
+        galaxy.takePicture();
+        galaxy.play();
     }
 }
