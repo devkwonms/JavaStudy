ae44272636c601333fdc046e10fda963b6fd10f5
devkwonms <alstj1379@nate.com>
2023-07-27 21:08:24 +0900

    defmodule

---
+++ src/main/java/com/example/chapter14/exam2.java
@@ -0,0 +1,11 @@
+package com.example.chapter14;
+
+import java.util.logging.Logger;
+
+public class exam2 {
+    public static void main(String[] args) {
+        Logger log = Logger.getLogger(exam2.class.getName());
+        log.info("Program Start");
+        log.warning("watch out");
+    }
+}
