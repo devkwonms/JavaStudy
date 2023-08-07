23b31b41beffb44cc1b0114458ad833040768460
devkwonms <alstj1379@nate.com>
2023-08-08 00:51:25 +0900

    finalize2

--- src/main/java/com/example/chapter15/exam4.java
+++ src/main/java/com/example/chapter15/exam4.java
@@ -8,17 +8,24 @@ class Network {
     void disconnect() {
         System.out.println("연결이 끊어졌습니다.");
     }
+
+    protected void finalize() throws Throwable {
+        super.finalize();
+        disconnect();
+    }
 }
 
 public class exam4 {
     public static void main(String[] args) {
 
         communication();
+        System.gc();    //  쓰레기 수집 강제지시 => 객체 즉시 제거
+        System.runFinalization();
     }
 
     static void communication() {
         Network net = new Network();
         net.connect();
-        net.disconnect();
+
     }
 }
