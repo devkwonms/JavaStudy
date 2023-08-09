50f67f80fd6f6925242bf38e6c8f5ab97038c54f
devkwonms <alstj1379@nate.com>
2023-08-10 02:06:30 +0900

    strLiteral(comment)

--- src/main/java/com/example/chapter15/exam6.java
+++ src/main/java/com/example/chapter15/exam6.java
@@ -2,8 +2,8 @@ package com.example.chapter15;
 
 public class exam6 {
     public static void main(String[] args) {
-        Integer wrapint = new Integer("629");
-
+//        Integer wrapint = new Integer("629"); // 사라질 방법이며, wrapper클래스를 생성자를 통해 만드는 것은 권장x
+        Integer wrapint = 629;
         String str = wrapint.toString();
         int i = Integer.parseInt(str);
         System.out.println(i);
