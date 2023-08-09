4927e88b1acb7eae4e9edd922bb21cead4729e97
devkwonms <alstj1379@nate.com>
2023-08-10 02:26:12 +0900

    autobox

---
+++ src/main/java/com/example/chapter15/exam9.java
@@ -0,0 +1,14 @@
+package com.example.chapter15;
+
+public class exam9 {
+    public static void main(String[] args) {
+        Integer a = new Integer(3);
+        Integer b = new Integer(4);
+        int c = a + b;
+        System.out.println("c = " + c);
+
+        Integer d = new Integer(12);
+        d++;
+        System.out.println("d = " + d);
+    }
+}
