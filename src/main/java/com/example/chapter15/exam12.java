eef8e14143f9bd25221b9f66826460e91a9deba1
devkwonms <alstj1379@nate.com>
2023-08-10 02:34:48 +0900

    nan

---
+++ src/main/java/com/example/chapter15/exam12.java
@@ -0,0 +1,9 @@
+package com.example.chapter15;
+
+public class exam12 {
+    public static void main(String[] args) {
+        System.out.println(3.14 / 0.0);
+        System.out.println(-3.14 / 0.0);
+        System.out.println(Math.sqrt(-2));
+    }
+}
