5a87771eed13da139d31023fd72790539f3b0891
devkwonms <alstj1379@nate.com>
2023-08-10 02:33:37 +0900

    floatinfo

---
+++ src/main/java/com/example/chapter15/exam11.java
@@ -0,0 +1,8 @@
+package com.example.chapter15;
+
+public class exam11 {
+    public static void main(String[] args) {
+        System.out.printf("Float의 크기 = %d, 지수 최솟값 = %d, 지수 최댓값 = %d\n", Float.SIZE, Float.MIN_EXPONENT, Float.MAX_EXPONENT);
+        System.out.printf("Double 크기 = %d, 지수 최솟값 = %d, 지수 최댓값 = %d\n", Double.SIZE, Double.MIN_EXPONENT, Double.MAX_EXPONENT);
+    }
+}
