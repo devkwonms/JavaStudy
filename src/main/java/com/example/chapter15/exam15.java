81e0957f5e3fdd10ca79b6bf9682df693c25fb8e
devkwonms <alstj1379@nate.com>
2023-08-10 03:13:54 +0900

    printfFormat

---
+++ src/main/java/com/example/chapter15/exam15.java
@@ -0,0 +1,14 @@
+package com.example.chapter15;
+
+public class exam15 {
+    public static void main(String[] args) {
+        int i = 123;
+        double d = 3.14159265;
+        System.out.printf("__%d__ \n", i);
+        System.out.printf("__%5d__ \n", i);
+        System.out.printf("__%-5d__ \n", i);
+        System.out.printf("__%05d__ \n", i);
+        System.out.printf("__%6.2f__ \n", d);
+        System.out.printf("__%8.4f__ \n", d);
+    }
+}
