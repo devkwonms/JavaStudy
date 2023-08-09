2b5bb7140bdbd7cf0085582fde4147ae853f31c4
devkwonms <alstj1379@nate.com>
2023-08-10 03:08:44 +0900

    printfIndex

--- src/main/java/com/example/chapter15/exam14.java
+++ src/main/java/com/example/chapter15/exam14.java
@@ -6,5 +6,7 @@ public class exam14 {
         double d = 3.14;
         String str = "문자열";
         System.out.printf(" i = %d, d = %f, str = %s\n", i, d, str);
+        System.out.printf(" i = %3$d, d = %2$f, str = %1$s\n", str, d, i);
+        System.out.printf(" 정수 = %1$d, i = %1$d", i);
     }
 }
