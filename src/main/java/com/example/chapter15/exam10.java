b20770fd609cf101817fe4c9b341167b15aaf583
devkwonms <alstj1379@nate.com>
2023-08-10 02:30:59 +0900

    intinfo

---
+++ src/main/java/com/example/chapter15/exam10.java
@@ -0,0 +1,9 @@
+package com.example.chapter15;
+
+public class exam10 {
+    public static void main(String[] args) {
+        System.out.printf("int 타입의 크기 = %d, 최솟값 = %d, 최댓값 = %d\n", Integer.SIZE, Integer.MIN_VALUE, Integer.MAX_VALUE);
+        System.out.printf("short 타입의 크기 = %d, 최솟값 = %d, 최댓값 = %d\n", Short.SIZE, Short.MIN_VALUE, Short.MAX_VALUE);
+        System.out.printf("long 타입의 크기 = %d, 최솟값 = %d, 최댓값 = %d\n", Long.SIZE, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+}
