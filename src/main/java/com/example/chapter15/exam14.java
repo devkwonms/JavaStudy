0d2a30847876f62d2b143ef1cacdac045c5dec70
devkwonms <alstj1379@nate.com>
2023-08-10 03:05:44 +0900

    printf

---
+++ src/main/java/com/example/chapter15/exam14.java
@@ -0,0 +1,10 @@
+package com.example.chapter15;
+
+public class exam14 {
+    public static void main(String[] args) {
+        int i = 123;
+        double d = 3.14;
+        String str = "문자열";
+        System.out.printf(" i = %d, d = %f, str = %s\n", i, d, str);
+    }
+}
