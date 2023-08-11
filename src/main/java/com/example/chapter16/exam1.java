5fddac08a7e1ca3795ab185889739c5ac98384a8
devkwonms <alstj1379@nate.com>
2023-08-11 17:56:20 +0900

    string의 hashcode와 equals 참조비교

---
+++ src/main/java/com/example/chapter16/exam1.java
@@ -0,0 +1,26 @@
+package com.example.chapter16;
+
+public class exam1 {
+    public static void main(String[] args) {
+        String a = "Y";
+        String b = "N";
+        String c = "Y";
+
+//        System.out.println(a.hashCode());
+//        System.out.println(b.hashCode());
+//        System.out.println(c.hashCode());
+
+//        System.out.println(a.equals(b));
+//        System.out.println(a.equals(c));
+
+        Integer d = 2;
+        Integer e = 3;
+        Integer f = new Integer(2);
+
+        System.out.println(d.equals(e));
+        System.out.println(d.equals(f));
+
+
+
+    }
+}
