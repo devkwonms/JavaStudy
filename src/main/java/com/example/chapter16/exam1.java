1347942ab0aba24bb87b768d216d05d40bb5b27f
devkwonms <alstj1379@nate.com>
2023-08-13 22:09:40 +0900

    stringctor

--- src/main/java/com/example/chapter16/exam1.java
+++ src/main/java/com/example/chapter16/exam1.java
@@ -2,25 +2,15 @@ package com.example.chapter16;
 
 public class exam1 {
     public static void main(String[] args) {
-        String a = "Y";
-        String b = "N";
-        String c = "Y";
+        String str = new String("아름다운");
+        System.out.println("str = " + str);
 
-//        System.out.println(a.hashCode());
-//        System.out.println(b.hashCode());
-//        System.out.println(c.hashCode());
+        String str2 = "우리나라";
+        System.out.println("str2 = " + str2);
 
-//        System.out.println(a.equals(b));
-//        System.out.println(a.equals(c));
-
-        Integer d = 2;
-        Integer e = 3;
-        Integer f = new Integer(2);
-
-        System.out.println(d.equals(e));
-        System.out.println(d.equals(f));
-
-
+        char[] ar = {'k', 'o', 'r', 'e', 'a'};
+        String str3 = new String(ar);
+        System.out.println("str3 = " + str3);
 
     }
 }
