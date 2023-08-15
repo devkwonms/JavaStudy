4c0c0d3bf1b730fa3cf83cefb790c8431c0a8719
devkwonms <alstj1379@nate.com>
2023-08-15 23:45:38 +0900

    trim

---
+++ src/main/java/com/example/chapter16/exam7.java
@@ -0,0 +1,10 @@
+package com.example.chapter16;
+
+public class exam7 {
+    public static void main(String[] args) {
+        String str = "      Kim Sang Hyung        ";
+
+        System.out.println(str.trim());
+        System.out.println(str.trim().concat(" BABO"));
+    }
+}
