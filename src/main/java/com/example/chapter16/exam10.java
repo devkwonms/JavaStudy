cf07f0f6c650eebcf2a9e1df5addd127d683c8b8
devkwonms <alstj1379@nate.com>
2023-08-16 01:07:16 +0900

    strassign

---
+++ src/main/java/com/example/chapter16/exam10.java
@@ -0,0 +1,13 @@
+package com.example.chapter16;
+
+public class exam10 {
+    public static void main(String[] args) {
+        String s = "korea";
+        String s2 = s;
+
+        s2 += " fighting";
+
+        System.out.println("s = " + s);
+        System.out.println("s2 = " + s2);
+    }
+}
