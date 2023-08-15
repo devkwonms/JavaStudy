97c406e767d0a4dfa59f1b5758f4c7e5cdd78ebb
devkwonms <alstj1379@nate.com>
2023-08-15 22:58:55 +0900

    stringLength

---
+++ src/main/java/com/example/chapter16/exam2.java
@@ -0,0 +1,9 @@
+package com.example.chapter16;
+
+public class exam2 {
+    public static void main(String[] args) {
+        String str = "아름다운 우리나라";
+        System.out.println("길이 : " + str.length());
+        System.out.println("2번째 문자 : " + str.charAt(1));
+    }
+}
