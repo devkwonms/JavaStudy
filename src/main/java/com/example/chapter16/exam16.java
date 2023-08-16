1d0f797803d75a777f7a1890368d4a1556cd6335
devkwonms <alstj1379@nate.com>
2023-08-17 01:56:02 +0900

    isBlank

---
+++ src/main/java/com/example/chapter16/exam16.java
@@ -0,0 +1,10 @@
+package com.example.chapter16;
+
+public class exam16 {
+    public static void main(String[] args) {
+        String text = "\t \n";
+        System.out.println("text = " + text);
+        System.out.println("text.isEmpty = " + text.isEmpty());
+        System.out.println("text.isBlank = " + text.isBlank());
+    }
+}
