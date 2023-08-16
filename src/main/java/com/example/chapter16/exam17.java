dc47a92e522a4d06576a4e21c19d860e49d3a05f
devkwonms <alstj1379@nate.com>
2023-08-17 01:57:15 +0900

    strip(앞뒤공백제거)

---
+++ src/main/java/com/example/chapter16/exam17.java
@@ -0,0 +1,9 @@
+package com.example.chapter16;
+
+public class exam17 {
+    public static void main(String[] args) {
+        String text = "   korea   ";
+        System.out.println("#" + text + "#");
+        System.out.println("#" + text.strip() + "#");
+    }
+}
