289dc84596864f27b50ee1ecb1eb284ccc9e0d9a
devkwonms <alstj1379@nate.com>
2023-08-17 01:35:30 +0900

    atoz(String)

---
+++ src/main/java/com/example/chapter16/exam13.java
@@ -0,0 +1,16 @@
+package com.example.chapter16;
+
+public class exam13 {
+    public static void main(String[] args) {
+        long start = System.currentTimeMillis();
+        String str = "";
+        for (int i = 0; i < 1000; i++) {
+            for (char ch = 'A'; ch <= 'z'; ch++) {
+                str += ch;
+            }
+            str += '\n';
+        }
+        System.out.println(str);
+        System.out.println((System.currentTimeMillis() - start) / 1000.0 + "초");
+    }
+}
