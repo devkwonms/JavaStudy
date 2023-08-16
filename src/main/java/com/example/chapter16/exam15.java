f151913adb3359c712c4219f1e0307f4950ffd8a
devkwonms <alstj1379@nate.com>
2023-08-17 01:43:59 +0900

    repeat

---
+++ src/main/java/com/example/chapter16/exam15.java
@@ -0,0 +1,10 @@
+package com.example.chapter16;
+
+public class exam15 {
+    public static void main(String[] args) {
+        String home = "대한민국 ";
+        String hurrah = "만세! ";
+
+        System.out.println(home + hurrah.repeat(3));
+    }
+}
