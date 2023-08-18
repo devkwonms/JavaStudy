7acfe38fdb1fd2f07bfa6955d0bcda9365eb4960
devkwonms <alstj1379@nate.com>
2023-08-19 00:27:19 +0900

    random2

---
+++ src/main/java/com/example/chapter16/exam29.java
@@ -0,0 +1,9 @@
+package com.example.chapter16;
+
+public class exam29 {
+    public static void main(String[] args) {
+        for(int i = 0; i < 5; i++){
+            System.out.println((int)(Math.random() * 12) + 1);
+        }
+    }
+}
