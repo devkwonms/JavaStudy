c6770dd134c2ceb08aed6cb3aa4275fce9df1494
devkwonms <alstj1379@nate.com>
2023-08-18 18:26:38 +0900

    indent

---
+++ src/main/java/com/example/chapter16/exam19.java
@@ -0,0 +1,9 @@
+package com.example.chapter16;
+
+public class exam19 {
+    public static void main(String[] args) {
+        String text = "<body>\n<p>sample</p>\n</body>";
+        System.out.println(text);
+        System.out.println(text.indent(4));
+    }
+}
