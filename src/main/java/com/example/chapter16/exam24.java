97298842f58159d13a4075032bfbf9ace378b97f
devkwonms <alstj1379@nate.com>
2023-08-18 22:10:13 +0900

    radian

---
+++ src/main/java/com/example/chapter16/exam24.java
@@ -0,0 +1,9 @@
+package com.example.chapter16;
+
+public class exam24 {
+    public static void main(String[] args) {
+        System.out.println("45도 = " + Math.toRadians(45));
+        System.out.println("1라디안 = " + Math.toDegrees(1));
+        System.out.println("3.14라디안 = " + Math.toDegrees(Math.PI));
+    }
+}
