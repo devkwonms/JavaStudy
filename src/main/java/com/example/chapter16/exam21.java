4c0bcc33e496706a2f578c37654f9d6c3f47ad67
devkwonms <alstj1379@nate.com>
2023-08-18 21:51:19 +0900

    mathconst

---
+++ src/main/java/com/example/chapter16/exam21.java
@@ -0,0 +1,8 @@
+package com.example.chapter16;
+
+public class exam21 {
+    public static void main(String[] args) {
+        System.out.println(Math.E);
+        System.out.println(Math.PI);
+    }
+}
