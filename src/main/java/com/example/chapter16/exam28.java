71ef7713f487aaea427d9b5090a65f62bf044646
devkwonms <alstj1379@nate.com>
2023-08-19 00:25:31 +0900

    random

---
+++ src/main/java/com/example/chapter16/exam28.java
@@ -0,0 +1,9 @@
+package com.example.chapter16;
+
+public class exam28 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            System.out.println(Math.random());
+        }
+    }
+}
