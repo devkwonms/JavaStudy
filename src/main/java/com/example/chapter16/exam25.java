72fbf26a4782bc92818e6d2139313a1968885401
devkwonms <alstj1379@nate.com>
2023-08-18 22:12:29 +0900

    sin

---
+++ src/main/java/com/example/chapter16/exam25.java
@@ -0,0 +1,9 @@
+package com.example.chapter16;
+
+public class exam25 {
+    public static void main(String[] args) {
+        for (double d = 0; d <= 90; d += 15){
+            System.out.println("sin(" + d + ") = " + Math.sin(Math.toRadians(d))) ;
+        }
+    }
+}
