c31552136613d2addbd621d42338380b81746f8f
devkwonms <alstj1379@nate.com>
2023-08-22 22:27:39 +0900

    asList

---
+++ src/main/java/com/example/chapter17/exam4.java
@@ -0,0 +1,14 @@
+package com.example.chapter17;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class exam4 {
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(11, 22, 33, 44, 55);
+        for (int i : list) {
+            System.out.print(i + " ");
+        }
+    }
+}
