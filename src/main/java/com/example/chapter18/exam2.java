253d33e7a6486d5674de683ee319c4390e78026e
devkwonms <alstj1379@nate.com>
2023-08-31 23:01:15 +0900

    genarraylist

---
+++ src/main/java/com/example/chapter18/exam2.java
@@ -0,0 +1,12 @@
+package com.example.chapter18;
+
+import java.util.*;
+
+public class exam2 {
+    public static void main(String[] args) {
+        ArrayList<Integer> arNum = new ArrayList<>();
+        arNum.add(1);
+        int value = arNum.get(0);
+        System.out.println(value);
+    }
+}
