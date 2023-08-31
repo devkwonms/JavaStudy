53afd65e2c465e2ee6338e76b1ce2160b9883dce
devkwonms <alstj1379@nate.com>
2023-08-31 23:51:43 +0900

    genbox

---
+++ src/main/java/com/example/chapter18/exam5.java
@@ -0,0 +1,29 @@
+package com.example.chapter18;
+
+class Box<T> {
+    T value;
+
+    Box(T value) {
+        this.value = value;
+    }
+
+    T get() {
+        return value;
+    }
+
+    void set(T value) {
+        this.value = value;
+    }
+}
+
+public class exam5 {
+    public static void main(String[] args) {
+        Box<Integer> bi = new Box<>(1234);
+        int biv = bi.get();
+        System.out.println(biv);
+
+        Box<Double> bd = new Box<>(56.78);
+        double bdv = bd.get();
+        System.out.println(bdv);
+    }
+}
