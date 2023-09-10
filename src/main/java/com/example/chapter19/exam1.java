1c895d88369c0ce84e006b6c5abc2089748427b3
devkwonms <alstj1379@nate.com>
2023-09-10 22:41:44 +0900

    ifdivide

---
+++ src/main/java/com/example/chapter19/exam1.java
@@ -0,0 +1,14 @@
+package com.example.chapter19;
+
+public class exam1 {
+    public static void main(String[] args) {
+        int a, b, c;
+        a = 2;
+        b = 0;
+        if (b != 0) {
+            c = a / b;
+            System.out.println("c = " + c);
+        }
+        System.out.println("프로그램 실행 완료");
+    }
+}
