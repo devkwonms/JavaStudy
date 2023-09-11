b919974309f7eff490bac05fac83caa24b5d5773
devkwonms <alstj1379@nate.com>
2023-09-11 18:52:56 +0900

    throw

---
+++ src/main/java/com/example/chapter19/exam7.java
@@ -0,0 +1,12 @@
+package com.example.chapter19;
+
+public class exam7 {
+    public static void main(String[] args) {
+        String name = "빌게이츠";
+        printScore(name);
+    }
+
+    static void printScore(String name) {
+        System.out.println(name.length());
+    }
+}
