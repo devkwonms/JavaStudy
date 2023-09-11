6b251c2f51ae09fc6d3a8ac55efbf17f2a68c1d7
devkwonms <alstj1379@nate.com>
2023-09-12 06:47:41 +0900

    networkclose

---
+++ src/main/java/com/example/chapter19/exam10.java
@@ -0,0 +1,14 @@
+package com.example.chapter19;
+
+public class exam10 {
+    public static void main(String[] args) {
+        System.out.println("접속");
+        try {
+            System.out.println("전송");
+            System.out.println("해제");
+        } catch (Exception e) {
+            System.out.println("예외 처리");
+            System.out.println("해제");
+        }
+    }
+}
