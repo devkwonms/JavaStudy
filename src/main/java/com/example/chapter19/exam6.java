a985d8e59e0acf4b9df5e2cd303b54cfaef028f1
devkwonms <alstj1379@nate.com>
2023-09-11 18:44:17 +0900

    stacktrace

---
+++ src/main/java/com/example/chapter19/exam6.java
@@ -0,0 +1,23 @@
+package com.example.chapter19;
+
+public class exam6 {
+    public static void main(String[] args) {
+        method();
+    }
+
+    static void method() {
+        submethod();
+    }
+
+    static void submethod() {
+        int i;
+        int a = 3, b = 0;
+        try {
+            i = a / b;
+            System.out.println(i);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+    }
+}
