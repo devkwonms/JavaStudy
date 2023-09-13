edb83ff8a62cc4a174baa8f42e385f653d5d65d9
devkwonms <alstj1379@nate.com>
2023-09-13 10:32:17 +0900

    thread

---
+++ src/main/java/com/example/chapter20/exam1.java
@@ -0,0 +1,30 @@
+package com.example.chapter20;
+
+public class exam1 {
+    public static void main(String[] args) {
+        PrintThread worker = new PrintThread();
+        worker.start();
+
+        for (int num = 0; num < 30; num++) {
+            System.out.print("O");
+            try {
+                Thread.sleep(200);
+            } catch (InterruptedException e) {
+                ;
+            }
+        }
+    }
+}
+
+class PrintThread extends Thread {
+    public void run() {
+        for (int num = 0; num < 30; num++) {
+            System.out.print("X");
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                ;
+            }
+        }
+    }
+}
