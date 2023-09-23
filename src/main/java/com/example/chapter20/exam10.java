145fd616541f2fbaa3c9ae8763edb6b681a431e4
devkwonms <alstj1379@nate.com>
2023-09-23 17:21:30 +0900

    interrupt2

--- src/main/java/com/example/chapter20/exam10.java
+++ src/main/java/com/example/chapter20/exam10.java
@@ -4,18 +4,26 @@ public class exam10 {
     public static void main(String[] args) {
         CalcThread worker = new CalcThread();
         worker.start();
+
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            ;
+        }
+        worker.interrupt();
     }
 }
 
 class CalcThread extends Thread {
     public void run() {
-        for (int i = 0; i < 10; i++) {
-            System.out.print(".");
-            try {
+        try {
+            for (int i = 0; i < 10; i++) {
+                System.out.print(".");
                 Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                ;
             }
+        } catch (InterruptedException e) {
+            System.out.println("\n계산 취소");
+            return;
         }
         System.out.println("\n계산 완료");
     }
