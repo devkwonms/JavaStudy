b581916bf32903a9124ad056efcc487074154344
devkwonms <alstj1379@nate.com>
2023-09-25 14:44:00 +0900

    blockread2

--- src/main/java/com/example/chapter21/exam3.java
+++ src/main/java/com/example/chapter21/exam3.java
@@ -5,11 +5,14 @@ import java.io.FileInputStream;
 public class exam3 {
     public static void main(String[] args) throws Exception {
         FileInputStream in = new FileInputStream("test.bin");
-        int data;
+        byte[] data = new byte[2];
+        int len;
         for (; ; ) {
-            data = in.read();
-            if (data == -1) break;
-            System.out.print(data);
+            len = in.read(data);
+            if (len == -1) break;
+            for (byte b : data) {
+                System.out.print(b);
+            }
         }
         in.close();
     }
