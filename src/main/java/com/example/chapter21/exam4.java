a5ca2e0f9cd092e08b2f1e2bba777cf115e35da6
devkwonms <alstj1379@nate.com>
2023-09-25 15:04:11 +0900

    textwrite

---
+++ src/main/java/com/example/chapter21/exam4.java
@@ -0,0 +1,33 @@
+package com.example.chapter21;
+
+import java.io.*;
+
+public class exam4 {
+    public static void main(String[] args) throws Exception {
+        String str = "자바 Stream 입출력";
+        FileWriter out = new FileWriter("test.txt");
+        out.write(str);
+        out.close();
+        System.out.println("Write success");
+
+        // 한 문자씩 읽기
+        FileReader in = new FileReader("test.txt");
+        int ch;
+        for (; ; ) {
+            ch = in.read();
+            if (ch == -1)
+                break;
+            System.out.print((char) ch);
+        }
+        in.close();
+        System.out.println();
+
+        // 한꺼번에 읽기
+        in = new FileReader("test.txt");
+        char[] text = new char[100];
+        int num = in.read(text);
+        System.out.println("읽은 문자 개수 = " + num);
+        System.out.println(text);
+        in.close();
+    }
+}
